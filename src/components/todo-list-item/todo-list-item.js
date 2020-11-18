d76fe12db7ace59ace1bad789b63688960d0aacf
PaulNorthern <sculltakers111@gmail.com>
2020-11-18 13:47:31 +0300

    Знакомство с SetStae

--- src/components/todo-list-item/todo-list-item.js
+++ src/components/todo-list-item/todo-list-item.js
@@ -4,33 +4,48 @@ import "./todo-list-item.css";
 // для работы с внутренним состоянием используем Class
 
 export default class TodoListItem extends Component {
+  // внутр. состояние
+  state = { done: false, important: false };
+
   onLabelClick = () => {
-    console.log(`Done: ${this.props.label}`);
+    // console.log(`Done: ${this.props.label}`);
+    this.setState({
+      done: true,
+    });
+  };
+
+  onMarkImportant = () => {
+    this.setState({
+      important: true,
+    });
   };
 
   render() {
     // деструктуризация - достать значения из объекта
     // который передается в качестве аргумента функции
-    const { label, important = false } = this.props;
+    const { label } = this.props;
+    const { done, important } = this.state;
 
-    const style = {
-      color: important ? "tomato" : "black",
-      fontWeight: important ? "bold" : "normal",
-    };
+    let classNames = "todo-list-item";
+
+    if (done) {
+      classNames += " done";
+    }
+
+    if (important) {
+      classNames += " important";
+    }
 
     return (
-      <span className="todo-list-item">
-        <span
-          className="todo-list-item-label"
-          style={style}
-          onClick={this.onLabelClick}
-        >
+      <span className={classNames}>
+        <span className="todo-list-item-label" onClick={this.onLabelClick}>
           {label}
         </span>
 
         <button
           type="button"
           className="btn btn-outline-success btn-sm float-right"
+          onClick={this.onMarkImportant}
         >
           <i className="fa fa-exclamation" />
         </button>
