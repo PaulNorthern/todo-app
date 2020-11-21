1c28401df348223184db6d001d358f782f319bc2
PaulNorthern <sculltakers111@gmail.com>
2020-11-21 15:33:22 +0300

    Фукнция Поиска onSearchChange и searc

--- src/components/item-add-form/item-add-form.js
+++ src/components/item-add-form/item-add-form.js
@@ -10,7 +10,7 @@ export default class ItemAddForm extends Component {
 
   onLabelChange = (e) => {
     this.setState({
-      label: e.target.value.toUpperCase(),
+      label: e.target.value,
     });
   };
 
