f9eb1cf014a08eb05a709ba16d6dc4462abbb10e
PaulNorthern <sculltakers111@gmail.com>
2020-11-21 14:59:11 +0300

    обработка формы

--- src/components/item-add-form/item-add-form.js
+++ src/components/item-add-form/item-add-form.js
@@ -4,16 +4,32 @@ import "./item-add-form.css";
 
 // rcc
 export default class ItemAddForm extends Component {
+  state = {
+    label: "",
+  };
+
+  onLabelChange = (e) => {
+    this.setState({
+      label: e.target.value,
+    });
+  };
+
+  onSubmit = (e) => {
+    e.preventDefault();
+    this.props.onItemAdded(this.state.label);
+  };
+
   render() {
     return (
-      <div className="item-add-form">
-        <button
-          className="btn btn-outline-secondary"
-          onClick={() => this.props.onItemAdded("Hello World")}
-        >
-          Add Item
-        </button>
-      </div>
+      <form className="item-add-form d-flex" onSubmit={this.onSubmit}>
+        <input
+          type="text"
+          className="form-control"
+          onChange={this.onLabelChange}
+          placeholder="What should to be done?"
+        />
+        <button className="btn btn-outline-secondary">Add Item</button>
+      </form>
     );
   }
 }
